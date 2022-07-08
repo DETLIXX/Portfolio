4b8f55c76ff2499df703d06ee37cd9deb75d45ba
Matus Balicky <43766321+DETLIXX@users.noreply.github.com>
2022-07-08 23:31:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-   console.log("Mobile Version")
-   window.location.href = 'mobile.html';
+    return;
+}
+else { 
+    console.log("Pc Version")
+    window.location.href = 'index.html';
 }
