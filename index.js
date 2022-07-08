316a3fab13b8d1459c1d7166bf9c12767b2816de
Matus Balicky <43766321+DETLIXX@users.noreply.github.com>
2022-07-09 00:29:10 +0200

    Redicting

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    return;
+if (navigator.appVersion.indexOf("Win") != -1) {
+    console.log("On Windows");
+    window.location.href= `index.html`;
 }
-else { 
-    console.log("Pc Version")
-    window.location.href = 'index.html';
+if (navigator.appVersion.indexOf("Mac") != -1) {
+    console.log("On MacOs");
+    window.location.href= `index.html`;
+}
+
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    window.location.href= `mobile.html`;
 }
