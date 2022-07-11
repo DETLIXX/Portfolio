1479656aff35f279feedf9a61deb3a025efe2893
Matus Balicky <43766321+DETLIXX@users.noreply.github.com>
2022-07-11 03:46:56 +0200

    Add files via upload

---
+++ js/homepage.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".support-item")[0].addEventListener("click",openInstagram);
+document.querySelectorAll(".support-item")[1].addEventListener("click",openGitHub);
+
+function openInstagram() { 
+    window.open("https://www.instagram.com/detlixx")
+}
+function openGitHub() { 
+    window.open("https://github.com/DETLIXX")
+}
+
