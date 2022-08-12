5e58f27e459d418c5eedaa6f595f9ff974093504
Matus Balicky <43766321+DETLIXX@users.noreply.github.com>
2022-08-12 17:37:34 +0200

    1737
    New Things

--- js/homepage.js
+++ js/homepage.js
@@ -7,3 +7,30 @@ function openInstagram() {
 function openGitHub() { 
     window.open("https://github.com/DETLIXX")
 }
+
+
+var i = 0;
+var innerText = document.getElementById("landingText").innerHTML; /* The text */
+var justText = document.getElementById("landingText").textContent; /* The text */
+var textLeng = justText.length;
+var speed = 25; /* The speed/duration of the effect in milliseconds */
+
+RemoveText();
+setTimeout(typeWriter,500);
+
+function RemoveText() { 
+    document.getElementById("landingText").textContent = "";
+}
+
+function typeWriter() { 
+    console.log(`${i} / ${textLeng}`);
+    if (i < justText.length) {
+        document.getElementById("landingText").innerHTML += justText.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+    if(i == textLeng) { 
+        document.getElementById("landingText").innerHTML = innerText;
+        $(".myImg").removeClass("hidden");
+    }
+}
